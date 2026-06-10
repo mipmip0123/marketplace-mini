9d3a2b52c22f8ae822edb925177381f027e3dd61
MhmdFzaz <muhfaizahza1990@gmail.com>
2026-06-10 18:26:00 +0700

    Merapikan alur kerja program dan integrasi type datanya

--- main.cpp
+++ main.cpp
@@ -2,52 +2,148 @@
 #include <stdlib.h>
 #include <fstream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
-//mengatur rupiah
-struct format_rupiah : numpunct<char>{
-    char do_thousands_sep() const {return '.';}
-    string so_grouping() const {return "\3";}
+// mengatur rupiah
+struct format_rupiah : numpunct<char>
+{
+    char do_thousands_sep() const { return '.'; }
+    string so_grouping() const { return "\3"; }
 };
 
-//database produk
-struct produk {
+// database produk
+struct produk
+{
     string nama;
     string spesifikasi;
     double harga;
     int stok;
 };
 
+// database akun
+//==============================
+const int MAX_AKUN = 100;
+int jml_akun = 0;
+
+string username[MAX_AKUN];
+string password[MAX_AKUN];
+
+char pilihan_login;
+
+// database produk
+//==============================
 const int MAX_PRODUK = 100;
 produk daftar_produk[MAX_PRODUK];
 int jml_produk = 0;
 
-void simpan_produk() {
-    ofstream file ("database_produk.txt");
-    if (!file)  {
+// type data untuk menu
+int pilihan = 0, pilihan_awal = 0, pilih = 0;
+
+// type data untuk kode penjual
+struct kode
+{
+    string kode_penjual;
+};
+
+bool login_kode;
+kode Codes[1] = {"11223344"};
+string kode_kode;
+
+// fungsi untuk login pembeli
+bool login(string user, string pass)
+{
+    for (int i = 0; i < jml_akun; i++)
+    {
+        if (username[i] == user && password[i] == pass)
+            return true;
+    }
+    return false;
+}
+// Fungction Menu Utama
+
+void menu_awal()
+{
+    cout << "======= MENU AWAL =======" << endl;
+    cout << "|  1.Sebagai Penjual    |" << endl;
+    cout << "|  2.Sebagai Pembeli    |" << endl;
+    cout << "|  3.keluar             |" << endl;
+    cout << "==========================" << endl;
+    cout << "Masukan pilihan anda : ";
+    cin >> pilihan_awal;
+    cin.ignore();
+    system("cls");
+}
+
+// Function Login Penjual
+void menu_penjual()
+{
+    cout << "======= MENU ADMIN =======" << endl;
+    cout << "|  1. Tambah Barang      |" << endl;
+    cout << "|  2. Hapus Barang       |" << endl;
+    cout << "|  3. Lihat data barang  |" << endl;
+    cout << "|  4. Keluar             |" << endl;
+    cout << "==========================" << endl;
+    cout << "Masukkan pilihan anda : ";
+    cin >> pilih;
+    system("cls");
+}
+
+// Function Produk Atau Menu penjual
+
+// Function Simpan Produk Ke database
+void simpan_produk()
+{
+    ofstream file("database_produk.txt");
+    if (!file)
+    {
         cout << "Gagal Membuka file untuk menyimpan!\n";
         return;
-    }  
-    for (int i = 0; i < jml_produk; i++) {
+    }
+    for (int i = 0; i < jml_produk; i++)
+    {
         file << daftar_produk[i].nama << "|";
         file << daftar_produk[i].spesifikasi << "|";
-        file << fixed << setprecision(0) << daftar_produk[i].harga << "|"; //membuat angka desimal dibulatkan dan menghapus semua angka di belakang koma
+        file << fixed << setprecision(0) << daftar_produk[i].harga << "|"; // membuat angka desimal dibulatkan dan menghapus semua angka di belakang koma
         file << daftar_produk[i].stok << "|" << endl;
     }
     file.close();
     cout << "Data produk berhasil disimpan\n";
 }
 
-int muat_produk() {
+string ambil_kolom(const string &line, int index)
+{
+    size_t awal = 0;
+    for (int i = 0; i < index; i++)
+    {
+        size_t pos = line.find("|", awal);
+        if (pos == string::npos)
+            return "";
+        awal = pos + 1;
+    }
+
+    size_t akhir = line.find("|", awal);
+    if (akhir == string::npos)
+        akhir = line.length();
+
+    return line.substr(awal, akhir - awal);
+}
+
+// Function Muat Produk Dari database
+int muat_produk()
+{
     ifstream file("database_produk.txt");
-    if (!file) {
+    if (!file)
+    {
         cout << ">> Database belum ada, mulai dengan data default.\n";
         return 0;
     }
     jml_produk = 0;
     string line;
-    while (getline(file, line)) {
-        if(line.empty()) continue;
+    while (getline(file, line))
+    {
+        if (line.empty())
+            continue;
 
         size_t pos1 = line.find("|");
         size_t pos2 = line.find("|", pos1 + 1);
@@ -56,16 +152,19 @@ int muat_produk() {
 
         daftar_produk[jml_produk].nama = line.substr(0, pos1);
         daftar_produk[jml_produk].spesifikasi = line.substr(pos1 + 1, pos2 - pos1 - 1);
-        daftar_produk[jml_produk].harga = stod(line.substr(pos2+1, pos3-pos2-1));
-        daftar_produk[jml_produk].stok  = stoi(line.substr(pos3+1, pos4-pos3-1));
+        daftar_produk[jml_produk].harga = stod(line.substr(pos2 + 1, pos3 - pos2 - 1));
+        daftar_produk[jml_produk].stok = stoi(line.substr(pos3 + 1, pos4 - pos3 - 1));
         jml_produk++;
     }
     file.close();
     return jml_produk;
 }
 
-void tambah_produk() {
-    if(jml_produk >= MAX_PRODUK) {
+// Function Tambah Produk
+void tambah_produk()
+{
+    if (jml_produk >= MAX_PRODUK)
+    {
         cout << "Database penuh tidak dapat menambah produk lagi\n";
         return;
     }
@@ -83,84 +182,37 @@ void tambah_produk() {
     simpan_produk();
 }
 
-void tampilkan_produk() {
-    if(jml_produk == 0) {
-        cout << "";
+// Function Tampilkan Produk
+void tampilkan_produk()
+{
+    if (jml_produk == 0)
+    {
+        cout << "Tidak ada produk yang tersedia.\n";
+        return;
     }
 
     cout.imbue(locale(cout.getloc(), new format_rupiah));
     cout << "========== DAFTAR PRODUK ==========" << endl;
-    for(int i = 0; i < jml_produk; i++){
+    for (int i = 0; i < jml_produk; i++)
+    {
         cout << i + 1 << "."
              << daftar_produk[i].nama
              << "|" << daftar_produk[i].spesifikasi
-             << "|" << fixed << setprecision(0) <<daftar_produk[i].harga
-             << "|" << daftar_produk[i].stok << endl;           
+             << "|" << fixed << setprecision(0) << daftar_produk[i].harga
+             << "|" << daftar_produk[i].stok << endl;
     }
-    cout << "===================================" << endl; 
+    cout << "===================================" << endl;
     cin.ignore();
     cout << "Tekan enter untuk kembali ke menu";
     cin.get();
     system("cls");
 }
 
+// Function Pembeli Atau Menu Pembeli
 
-//login pengguna
-const int MAX_AKUN =100;
-int jml_akun = 0;
-
-string username[MAX_AKUN];
-string password[MAX_AKUN];
-
-char pilihan_login;
-
-void simpan_akun() {
-    ofstream file ("database_akun.txt");
-    if (!file) {
-        cout << "Gagal Membuka file untuk menyimpan!\n";
-        return;
-    }
-    for (int i = 0; i < jml_akun; i++) {
-        file << username[i] << "|" << password[i] << endl;
-    }
-    file.close();
-    cout << "Data akun berhasil disimpan" << endl;
-}
-
-int muat_akun() {
-    ifstream file("database_akun.txt");
-    if (!file) {
-        cout << ">> Database belum ada, mulai dengan data default.\n";
-        return 0;
-    }
-    jml_akun = 0;
-    while (getline(file, username[jml_akun], '|') && 
-    getline(file, password[jml_akun])) {
-        jml_akun++;
-    }
-    file.close();
-    return jml_akun;
-}
-
-void tambah_akun() {
-    cout << "Masukan username : ";
-    getline(cin, username[jml_akun]);
-    cout << "Masukkka password : ";
-    getline(cin, password[jml_akun]);
-    jml_akun++;
-    simpan_akun();
-    system("cls");
-    cout << "Selamat anda berhasil mendaftar, selamat berbelanja" << endl;
-}
-
-bool login(string user, string pass){
-    for(int i = 0; i < jml_akun; i++){
-        if(username[i] == user && password[i] == pass) return true;
-    }
-    return false;
-}
-
-void menu_login(){
+// Function Menu Login Pembeli
+void menu_login_buyer()
+{
     cout << "======= MENU LOGIN =======" << endl;
     cout << "|       1.Login          |" << endl;
     cout << "|       2.Register       |" << endl;
@@ -172,96 +224,113 @@ void menu_login(){
     system("cls");
 }
 
-//menu awal
-int pilihan_awal;
-void menu_awal(){
-    cout << "======= MENU AWAL =======" << endl;
-    cout << "|  1.Sebagai Penjual    |" << endl;
-    cout << "|  2.Sebagai Pembeli    |" << endl;
-    cout << "|  3.keluar             |" << endl;
+// Function Simpan Akun Ke database
+void simpan_akun()
+{
+    ofstream file("database_akun.txt");
+    if (!file)
+    {
+        cout << "Gagal Membuka file untuk menyimpan!\n";
+        return;
+    }
+    for (int i = 0; i < jml_akun; i++)
+    {
+        file << username[i] << "|" << password[i] << endl;
+    }
+    file.close();
+    cout << "Data akun berhasil disimpan" << endl;
+}
+
+// Function Muat Akun Dari database
+int muat_akun()
+{
+    ifstream file("database_akun.txt");
+    if (!file)
+    {
+        cout << ">> Database belum ada, mulai dengan data default.\n";
+        return 0;
+    }
+    jml_akun = 0;
+    while (getline(file, username[jml_akun], '|') &&
+           getline(file, password[jml_akun]))
+    {
+        jml_akun++;
+    }
+    file.close();
+    return jml_akun;
+}
+
+// Function Tambah Akun
+void tambah_akun()
+{
+    cout << "Masukan username : ";
+    getline(cin, username[jml_akun]);
+    cout << "Masukkka password : ";
+    getline(cin, password[jml_akun]);
+    jml_akun++;
+    simpan_akun();
+    system("cls");
+    cout << "Selamat anda berhasil mendaftar, selamat berbelanja" << endl;
+}
+
+// Function Menu Utama Pembeli
+void menu_utama()
+{
+    cout << "======= MENU UTAMA =======" << endl;
+    cout << "|  1. Lihat Produk       |" << endl;
+    cout << "|  2. Cari Produk        |" << endl;
+    cout << "|  3. Tambah ke Keranjang|" << endl;
+    cout << "|  4. Lihat Keranjang    |" << endl;
+    cout << "|  5. Hapus dari Keranjang|" << endl;
+    cout << "|  6. Checkout           |" << endl;
+    cout << "|  7. Riwayat Pembelian   |" << endl;
+    cout << "|  8. Logout             |" << endl;
+    cout << "|  9. Keluar             |" << endl;
     cout << "==========================" << endl;
     cout << "Masukan pilihan anda : ";
-    cin >> pilihan_awal;
+    cin >> pilihan;
     cin.ignore();
     system("cls");
 }
 
-//menu penjual
-struct kode{
-    string kode_penjual;
-};
+int main()
+{
 
-bool login_kode;
-kode Codes[1] ={"11223344"};
-string kode_kode;
-
-void menu_penjual(){
-    bool login_kode = false;
-    do {
-    cout << "======= MENU PENJUAL =======" << endl;
-    cout << "Masukkan Kode : ";
-    getline(cin, kode_kode);
-    system("cls");
-    
-        
-    //CEK KODE BERHASIL
-        if (kode_kode == Codes[0].kode_penjual) {
-            login_kode = true;
-            cout << "================ Login Berhasil ================" << endl;
-            cout << "|            Selamat datang Admin!             |" << endl;
-            cout << "================================================";
-            cin.get();
-            system("cls");
-
-        } else {
-            cout << "================= Kode Gagal =================" << endl;
-            cout << "|       Kode yang anda masukan salah!        |" << endl;
-            cout << "|                 Coba lagi!                 |" << endl; 
-            cout << "==============================================";
-            cin.get();
-            system("cls");
-        }
-    } while (!login_kode);   
-
-    //menu admin penjual
-            int pilih;
-            do {
-                cout << "======= MENU ADMIN =======" << endl;
-                cout << "|  1. Tambah Barang      |" << endl;
-                cout << "|  2. Hapus Barang       |" << endl;
-                cout << "|  3. Lihat data barang  |" << endl;
-                cout << "|  4. Keluar             |" << endl;
-                cout << "==========================" << endl;
-                cout << "Masukkan pilihan anda : ";
-                cin >> pilih;
-                system("cls");
-
-                switch (pilih)
-                {
-                case 1: tambah_produk(); break;
-                case 2: break;
-                case 3: tampilkan_produk(); break;
-                case 4: system("cls"); break;
-                
-                default:
-                    break;
-                }
-            }while(pilih != 4);
-}
-
-
-int main() {
-    int pilihan;
-
-    //menu awal
-    menu_awal :
-    do {
+// menu awal
+menu_awal:
+    do
+    {
         menu_awal();
         switch (pilihan_awal)
         {
         case 1:
-            menu_penjual();
+        menu_login_penjual:
+            cout << "======= MENU PENJUAL =======" << endl;
+            cout << "Masukkan Kode : ";
+            getline(cin, kode_kode);
+            system("cls");
 
+            // CEK KODE BERHASIL
+            if (kode_kode == Codes[0].kode_penjual)
+            {
+                login_kode = true;
+                cout << "================ Login Berhasil ================" << endl;
+                cout << "|            Selamat datang Admin!             |" << endl;
+                cout << "================================================";
+                cin.get();
+                system("cls");
+                goto menu_penjual;
+            }
+            else
+            {
+                cout << "================= Kode Gagal =================" << endl;
+                cout << "|       Kode yang anda masukan salah!        |" << endl;
+                cout << "|                 Coba lagi!                 |" << endl;
+                cout << "==============================================";
+                cin.get();
+                system("cls");
+                goto menu_login_penjual;
+            }
             break;
         case 2:
             goto menu_login;
@@ -270,116 +339,132 @@ int main() {
         case 3:
             return 0;
             break;
-        
+
         default:
-        cout << "============================****=============================" << endl;
-        cout << "|      pilihan salah, tekan enter untuk coba lagi           |" << endl;
-        cout << "============================****=============================";
-        cin.get ();
-        system ("cls");
-        goto menu_awal;
-        break;
+            cout << "============================****=============================" << endl;
+            cout << "|      pilihan salah, tekan enter untuk coba lagi           |" << endl;
+            cout << "============================****=============================";
+            cin.get();
+            system("cls");
+            goto menu_awal;
+            break;
             break;
         }
-    }while(pilihan_awal != 3);
-     
-    //menu pembeli
-    muat_akun();
-    menu_login :
-    do {
-    menu_login();
-    switch (pilihan_login)
+    } while (pilihan_awal != 3);
+
+    // menu penjual
+menu_penjual:
+    do
     {
-    case '1': {
-        string user, pass;
-        cout << "Masukkan username : ";
-        getline(cin, user);
-        cout << "Masukkan Password :";
-        getline(cin, pass);
-        
-        if(login(user, pass)){
-            cout << "Login berhasil" << endl;
-            cout << "Selamat berbelanja" << endl;
-        } else {
-            cout << "Login Gagal" << endl;
-            cout << "Silahkan coba lagi" << endl;
+        menu_penjual();
+        switch (pilih)
+        {
+        case 1:
+            tambah_produk();
+            break;
+        case 2:
+            break;
+        case 3:
+            tampilkan_produk();
+            break;
+        case 4:
+            system("cls");
+            break;
+
+        default:
+            break;
         }
-        system("cls");
-        cout << "Selamat datang di marketplace mini"<< endl;
-        cout << "Tekan enter untuk melanjutkan";
-        cin.get();
-        goto menu_utama;
-        system("cls");
-        break;
-    }
-    case '2':
-        tambah_akun();
-        break;
-    case '3':
-        return 0;
-        
-    default:
-        cout << "============================****=============================" << endl;
-        cout << "|      pilihan salah, tekan enter untuk coba lagi           |" << endl;
-        cout << "============================****=============================";
-        cin.get ();
-        system ("cls");
-        goto menu_login;
-        break;
-    }
-    }while( pilihan_login != 3);
+    } while (pilih != 4);
 
-    //menu utama
-    menu_utama :
-    do {
-        cout << "=========== MARKETPLACE HANDPHONE ==========" << endl;
-        cout << "|            1.Tambah data                 |" << endl;
-        cout << "|            2.Tampilkan data              |" << endl;
-        cout << "|            3.Updtate data                |" << endl;
-        cout << "|            4.Hapus data                  |" << endl;
-        cout << "|            5.Cari data                   |" << endl;
-        cout << "|            6.Urutkan data                |" << endl;
-        cout << "|            7.Simulasi stack              |" << endl;
-        cout << "|            8.Simulasi queue              |" << endl;
-        cout << "|            9.Keluar                      |" << endl;
-        cout << "============================================" << endl;
-        cout << "Masukan pilihan anda : ";
-        cin >> pilihan;
+    // menu pembeli
+    muat_akun();
+menu_login:
+    do
+    {
+        menu_login_buyer();
+        switch (pilihan_login)
+        {
+        case '1':
+        {
+            string user, pass;
+            cout << "Masukkan username : ";
+            getline(cin, user);
+            cout << "Masukkan Password :";
+            getline(cin, pass);
 
+            if (login(user, pass))
+            {
+                cout << "Login berhasil" << endl;
+                cout << "Selamat berbelanja" << endl;
+            }
+            else
+            {
+                cout << "Login Gagal" << endl;
+                cout << "Silahkan coba lagi" << endl;
+            }
+            system("cls");
+            cout << "Selamat datang di marketplace mini" << endl;
+            cout << "Tekan enter untuk melanjutkan";
+            cin.get();
+            goto menu_utama;
+            system("cls");
+            break;
+        }
+        case '2':
+            tambah_akun();
+            break;
+        case '3':
+            return 0;
+
+        default:
+            cout << "============================****=============================" << endl;
+            cout << "|      pilihan salah, tekan enter untuk coba lagi           |" << endl;
+            cout << "============================****=============================";
+            cin.get();
+            system("cls");
+            goto menu_login;
+            break;
+        }
+    } while (pilihan_login != 3);
+
+// menu utama
+menu_utama:
+    do
+    {
+        menu_utama();
         switch (pilihan)
         {
         case 1:
-            
+
             break;
         case 2:
-            
+
             break;
         case 3:
-            
+
             break;
         case 4:
-            
+
             break;
         case 5:
-            
+
             break;
         case 6:
-            
+
             break;
         case 7:
-            
+
             break;
         case 8:
-            
+
             break;
         case 9:
             cout << "\nTerimakasih" << endl;
             break;
-        
+
         default:
             break;
         }
 
-    } while(pilihan != 9);
-    
+    } while (pilihan != 9);
 }
